93ad5d743983d93b29d0609c46cf21479218b203
DarienTsai <smartdragondt@gmail.com>
2018-08-29 21:37:21 -0700

    day one

---
+++ signup.js
@@ -0,0 +1,61 @@
+//DOM for the signup popup
+let d=document;
+let navSign = d.getElementById('sign-a');
+let sign = d.getElementById('log');
+
+navSign.addEventListener(
+    'click',
+    signUp
+);
+
+sign.addEventListener(
+    'click',
+    signUp
+);
+
+let pop = d.getElementById('sign');
+let close = d.getElementById('close');
+
+function signUp() {
+    pop.className = "active";
+}
+
+close.addEventListener(
+    'click',
+    function(){
+        pop.className = "off";
+    },
+);
+
+let upp = d.getElementById('upp');
+let inns = d.getElementById('inn');
+let up = d.getElementById('up');
+let ins = d.getElementById('in');
+let over = d.getElementById('over');
+
+upp.addEventListener('click',
+function(){
+    ins.className="off";
+    inns.className="";
+    up.className="formed";
+    upp.className="on";
+}
+);
+
+inns.addEventListener('click',
+function(){
+    up.className="off";
+    upp.className="";
+    ins.className="formed";
+    inns.className="on";
+}
+);
+
+let start = d.getElementById('start-button');
+let bod = d.getElementById('bod');
+
+start.addEventListener('click',
+function(){
+    over.className="in";
+    setTimeout(function(){location.assign("home.html");}, 1000);
+});
